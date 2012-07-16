6d5d108698512dc795fc6c6ff852177003c13ba0
nlabrot <nicolas.labrot@4dconcept.fr>
2012-07-16 14:26:46 +0200

    Embed inheritance script

--- jsf-component/src/main/javascript/jsf.dynatree.adapter.js
+++ jsf-component/src/main/javascript/jsf.dynatree.adapter.js
@@ -1,6 +1,73 @@
-var Dynatree4Jsf = $.inherit({
+/* Simple JavaScript Inheritance
+ * By John Resig http://ejohn.org/
+ * MIT Licensed.
+ */
+// Inspired by base2 and Prototype
+(function(){
+    var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
 
-    __constructor:function (htmlId, clientId, options) {
+    // The base DynaClass implementation (does nothing)
+    this.DynaClass = function(){};
+
+    // Create a new DynaClass that inherits from this class
+    DynaClass.extend = function(prop) {
+        var _super = this.prototype;
+
+        // Instantiate a base class (but only create the instance,
+        // don't run the init constructor)
+        initializing = true;
+        var prototype = new this();
+        initializing = false;
+
+        // Copy the properties over onto the new prototype
+        for (var name in prop) {
+            // Check if we're overwriting an existing function
+            prototype[name] = typeof prop[name] == "function" &&
+                typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+                (function(name, fn){
+                    return function() {
+                        var tmp = this._super;
+
+                        // Add a new ._super() method that is the same method
+                        // but on the super-class
+                        this._super = _super[name];
+
+                        // The method only need to be bound temporarily, so we
+                        // remove it when we're done executing
+                        var ret = fn.apply(this, arguments);
+                        this._super = tmp;
+
+                        return ret;
+                    };
+                })(name, prop[name]) :
+                prop[name];
+        }
+
+        // The dummy class constructor
+        function DynaClass() {
+            // All construction is actually done in the init method
+            if ( !initializing && this.init )
+                this.init.apply(this, arguments);
+        }
+
+        // Populate our constructed prototype object
+        DynaClass.prototype = prototype;
+
+        // Enforce the constructor to be what we expect
+        DynaClass.prototype.constructor = DynaClass;
+
+        // And make this class extendable
+        DynaClass.extend = arguments.callee;
+
+        return DynaClass;
+    };
+})();
+
+
+
+var Dynatree4Jsf = DynaClass.extend({
+
+    init:function (htmlId, clientId, options) {
         this.clientId = clientId;
         this.htmlId = htmlId;
         this.options = options;
